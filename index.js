55b85be1b4428a1dd06977f0e03109178f6cb213
Jayvee Perez <Jayvee.Perez@cebupacificair.com>
2021-11-22 15:21:42 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/dissectKey', (req, res) => {
+    var items = req.query.key.split('');
+    var list1 = items.filter(item => /[A-Za-z]/.test(item));
+    var list2 = items.filter(item => /[6-9]/.test(item));
+    
+    res.json({
+        list1,
+        list2 
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at port: ${port}`);
+});
