9ccc6070409529b6199ab85c321cd9cdfb7091c6
Jayvee U. Perez <jayveperez13@gmail.com>
2022-05-15 15:55:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ const port = process.env.PORT || 3000;
 const classicCards = require('./classic-cards');
 const originCards = require('./origin-cards');
 
-app.get('/dissectKey', (req, res) => {
-    var items = req.query.key.split('');
-    var list1 = items.filter(item => /[A-Za-z]/.test(item));
-    var list2 = items.filter(item => /[6-9]/.test(item));
-    
-    res.json({
-        list1,
-        list2 
-    });
-});
-
 app.get('/classic-cards', (req, res) => {
     res.json(classicCards);
 });
